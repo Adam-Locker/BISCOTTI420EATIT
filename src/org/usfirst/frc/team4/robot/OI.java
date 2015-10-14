ef2533384ed3e228135f7b4f1945e7fc071bcb2e
Adam-Locker <adam.locker@ht-la.org>
2015-10-14 16:47:21 -0700

    Initiate Biscotti420EATIT

---
+++ src/org/usfirst/frc/team4/robot/OI.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team4.robot;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+
+Joystick exampleJoy = new Joystick(1);
+
+Button exampleButton = new JoystickButton(exampleJoy,1);
+}
+
